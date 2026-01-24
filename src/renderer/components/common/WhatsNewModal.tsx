c12d34b5b90d66762cd624a8e3fb765108409e87
angelo <angelontoto7@gmail.com>
2026-01-24 18:10:03 +0100

    Fix: Ajout de l'icône Rocket et correction du chemin d'import

--- src/renderer/components/common/WhatsNewModal.tsx
+++ src/renderer/components/common/WhatsNewModal.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { X, Sparkles, ArrowRight, Check, Rocket } from '../iconsSvg';
 import { changelog, ReleaseNote } from '../../data/changelog';
-import pkg from '../../../../package.json'
+import pkg from '../../../../package.json';
 interface WhatsNewModalProps {
   onClose?: () => void;
 }
--- src/renderer/components/iconsSvg.tsx
+++ src/renderer/components/iconsSvg.tsx
@@ -638,3 +638,9 @@ export const Mail = createIcon("Mail", [
   ["rect", { width: "20", height: "16", x: "2", y: "4", rx: "2", key: "mail1" }],
   ["path", { d: "m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7", key: "mail2" }]
 ]);
+export const Rocket = createIcon("Rocket", [
+  ["path", { d: "M4.5 16.5c-1.5 1.26-2 5-2 5s3.74-.5 5-2c.71-.84.7-2.13-.09-2.91a2.18 2.18 0 0 0-2.91-.09z", key: "rocket1" }],
+  ["path", { d: "m12 15-3-3a22 22 0 0 1 2-3.95A12.88 12.88 0 0 1 22 2c0 2.72-.78 7.5-6 11a22.35 22.35 0 0 1-4 2z", key: "rocket2" }],
+  ["path", { d: "M9 12H4s.55-3.03 2-4c1.62-1.08 5 0 5 0", key: "rocket3" }],
+  ["path", { d: "M12 15v5s3.03-.55 4-2c1.08-1.62 0-5 0-5", key: "rocket4" }]
+]);
